979be66a2cca1f4bb7d0ff9c4437ced9a0b611c8
Abdel Rahman Ali <43393558+abdelrahman48@users.noreply.github.com>
2019-09-29 02:49:21 +0200

    forgot to upload the gulpfile!

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var { src, dest, watch, parallel  } = require('gulp');
+var { src, dest, watch, parallel, series } = require('gulp');
 
 // html related plugins
 var pug = require('gulp-pug');
@@ -6,6 +6,16 @@ var pug = require('gulp-pug');
 // CSS related plugins
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var postcss = require('gulp-postcss');
+var uncss = require('postcss-uncss');
+var cssnano = require('gulp-cssnano');
+var plugins = [
+    uncss({
+        html: ['build/index.html'],
+        ignore : [/\.active/],
+        timeout: 1000
+    })
+];
 
 // JS related plugins
 var uglify = require('gulp-uglify');
@@ -22,20 +32,26 @@ var connect = require('gulp-connect');
 function html() {
     return src('src/index.pug')
         .pipe(pug({pretty: true}))
-        .pipe(dest('build'))
+        .pipe(dest('build/'))
         .pipe(connect.reload())
 }
 
 function css() {
     return src('src/css/sass/main.sass')
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'expanded'}))
         .pipe(autoprefixer('last 2 versions'))
-        .pipe(dest('build/css'))
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(dest('build/css'))
+        .pipe(sourcemaps.write('./map'))
+        .pipe(dest('build/css/'))
+        .pipe(connect.reload())
+}
+
+function allcss() {
+    return src(['build/css/main.css', 'build/vendor/css/*.css'])
+        .pipe(concat('all.min.css'))
+        .pipe(postcss(plugins))
+        .pipe(cssnano())
+        .pipe(dest('build/css/'))
         .pipe(connect.reload())
 }
 
@@ -43,36 +59,36 @@ function js() {
     return src('src/js/*.js')
         .pipe(sourcemaps.init())
         .pipe(concat('main.js'))
-        .pipe(dest('build/js'))
+        .pipe(dest('build/js/'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(dest('build/js'))
+        .pipe(sourcemaps.write('./map'))
+        .pipe(dest('build/js/'))
         .pipe(connect.reload())
 }
 
 function img() {
     return src('src/img/*.*')
         .pipe(imagemin({verbose: true}))
-        .pipe(dest('build/img'))
+        .pipe(dest('build/img/'))
         .pipe(connect.reload())
 }
 
 function vendor() {
-    return src('src/vendor/*.*')
-        .pipe(dest('build/vendor'))
+    return src('src/vendor/**')
+        .pipe(dest('build/vendor/'))
         .pipe(connect.reload())
 }
 
 function font() {
     return src('src/webfonts/*.*')
-        .pipe(dest('build/webfonts'))
+        .pipe(dest('build/webfonts/'))
         .pipe(connect.reload())
 }
 
 function watcher(done) {
     watch('src/**/*.pug', html);
-    watch(['src/**/*.sass', 'src/**/*.css'], css);
+    watch('src/**/*.sass', css);
     watch('src/**/*.js', js);
     watch('src/img/**', img);
     watch('src/webfonts/**', font);
@@ -90,3 +106,10 @@ function LiveReload(done) {
 }
 
 exports.default = parallel(LiveReload, watcher);
+exports.html = html;
+exports.css = series(css, allcss);
+exports.js = js;
+exports.img = img;
+exports.font = font;
+exports.vendor = vendor;
+exports.all = series(html, js, css, img, font, vendor);
